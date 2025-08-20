50e821c91b3dbc55595ea1a0cd2b377bdbfc8495
rthakkar0555 <rthakkar0555@gmail.com>
2025-08-20 10:26:59 +0530

    nn

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    
   ].filter(Boolean),
   resolve: {
     alias: {
